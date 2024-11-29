86fea37890fca46ab10ff130f54dc0dcaa42585e
melissafilomeno <melissalfilomeno@gmail.com>
2024-11-29 11:31:36 -0500

    init

---
+++ src/main/java/com/myorg/bugTrackerWebFluxPoc/BugTrackerWebfluxPocApplication.java
@@ -0,0 +1,13 @@
+package com.myorg.bugTrackerWebFluxPoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTrackerWebfluxPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugTrackerWebfluxPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myorg/bugTrackerWebFluxPoc/BugTrackerWebfluxPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myorg.bugTrackerWebFluxPoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugTrackerWebfluxPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
